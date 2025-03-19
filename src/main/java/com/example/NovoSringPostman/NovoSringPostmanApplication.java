16f0a20d1913d97e4b08be30cbf1ae6e37335bc1
Aluno Dendezeiros <aluno.den@senaiba.com.br>
2025-03-19 19:18:52 -0300

    first commit

---
+++ src/main/java/com/example/NovoSringPostman/NovoSringPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.NovoSringPostman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoSringPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoSringPostmanApplication.class, args);
+	}
+
+}
